c09b56c5a5037dcbb7692d18cbbe77a287dd2377
Adam Shannon <adamkshannon@gmail.com>
2019-03-15 11:20:47 -0500

    cmd/server: respond with CORS headers on 'GET /ping'

---
+++ cmd/server/http_test.go
@@ -0,0 +1,35 @@
+// Copyright 2019 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+)
+
+func TestRouting_ping(t *testing.T) {
+	router := mux.NewRouter()
+	addPingRoute(router)
+
+	req := httptest.NewRequest("GET", "/ping", nil)
+	req.Header.Set("Origin", "https://moov.io")
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus HTTP status: %d", w.Code)
+	}
+	if v := w.Body.String(); v != "PONG" {
+		t.Errorf("body: %s", v)
+	}
+	if v := w.Result().Header.Get("Access-Control-Allow-Origin"); v != "https://moov.io" {
+		t.Errorf("Access-Control-Allow-Origin: %s", v)
+	}
+}
