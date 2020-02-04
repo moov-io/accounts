a9364ed408aeb7336a62b6b319227ce60e38221b
Adam Shannon <adamkshannon@gmail.com>
2020-02-04 11:35:22 -0600

    api,client: wait, does openapi-generator merge idential files?

--- client/model_error.go
+++ client/model_error.go
@@ -9,8 +9,8 @@
 
 package openapi
 
-// AccountError struct for AccountError
-type AccountError struct {
+// Error struct for Error
+type Error struct {
 	// An error message describing the problem intended for humans.
 	Error string `json:"error"`
 }
